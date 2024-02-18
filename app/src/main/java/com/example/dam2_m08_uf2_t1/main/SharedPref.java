508d9d8728c0b676c4cddbab506712ab130bffdc
Nando1702 <120483561+Nando1702@users.noreply.github.com>
2024-02-18 21:31:07 +0100

    refactor

--- app/src/main/java/com/example/dam2_m08_uf2_t1/main/SharedPref.java
+++ app/src/main/java/com/example/dam2_m08_uf2_t1/main/SharedPref.java
@@ -7,18 +7,24 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SharedPref {
+
+    // Nombre del archivo de preferencias compartidas
     private static final String PREF_NAME = "BicingPrefs";
+    // Clave para las ubicaciones favoritas en las preferencias compartidas
     private static final String KEY_FAVORITE_LOCATIONS = "favorite";
+
+    // Obtiene el objeto SharedPreferences
     private static SharedPreferences getSharedPreferences(Context context) {
         return context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
     }
+    // Agrega una ubicación favorita al conjunto de ubicaciones favoritas.
     public static void addFavoriteLocation(Context context, int locationId) {
         Log.d("SharedPref", "Adding location to favorites: " + locationId);
         Set<String> favoriteLocations = getFavoriteLocationsSet(context);
         favoriteLocations.add(String.valueOf(locationId));
         saveFavoriteLocations(context, favoriteLocations);
     }
-
+    // Elimina una ubicación favorita del conjunto de ubicaciones favoritas.
     public static void removeFavoriteLocation(Context context, int locationId) {
         Log.d("SharedPref", "Removing location from favorites: " + locationId);
         Set<String> favoriteLocations = getFavoriteLocationsSet(context);
@@ -26,6 +32,7 @@ public class SharedPref {
         saveFavoriteLocations(context, favoriteLocations);
     }
 
+    // Obtiene el conjunto de ubicaciones favoritas como un conjunto de enteros.
     public static Set<Integer> getFavoriteLocationsInt(Context context) {
         Log.d("SharedPref", "Getting favorite locations");
         Set<String> stringSet = getFavoriteLocationsSet(context);
@@ -37,12 +44,12 @@ public class SharedPref {
 
         return integerSet;
     }
-
+    // Obtiene el conjunto de ubicaciones favoritas como un conjunto de cadenas.
     private static Set<String> getFavoriteLocationsSet(Context context) {
         SharedPreferences preferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
         return preferences.getStringSet(KEY_FAVORITE_LOCATIONS, new HashSet<String>());
     }
-
+    // Guarda el conjunto de ubicaciones favoritas en las preferencias compartidas.
     private static void saveFavoriteLocations(Context context, Set<String> favoriteLocations) {
         SharedPreferences preferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
