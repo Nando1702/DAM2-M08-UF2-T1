f56f21fb79f8db2058f0bde5e22442b8049134d6
Nando1702 <120483561+Nando1702@users.noreply.github.com>
2024-02-18 21:03:19 +0100

    refactor

--- app/src/main/java/com/example/dam2_m08_uf2_t1/recyclerView/RecyclerViewInterface.java
+++ app/src/main/java/com/example/dam2_m08_uf2_t1/recyclerView/RecyclerViewInterface.java
@@ -1,5 +1,17 @@
 package com.example.dam2_m08_uf2_t1.recyclerView;
 public interface RecyclerViewInterface {
+
+    /**
+     * Método llamado cuando se hace clic en un elemento de la lista.
+     *
+     * @param position La posición del elemento en la lista que se ha clicado.
+     */
     void onItemCLick(int position);
+
+    /**
+     * Método llamado cuando se hace clic en un botón dentro de un elemento de la lista.
+     *
+     * @param position La posición del elemento en la lista que contiene el botón que se ha clicado.
+     */
     void onButtonCLick(int position);
 }
